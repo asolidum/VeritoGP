7a6be5277472b41569021449db9313f0ebb41834
Alan Solidum <hi@alansolidum.com>
2021-09-16 18:36:01 -0700

    feat: Update main() to display all valid media filenames in curr dir

--- src/com/alansolidum/veritogp/VeritoGP.java
+++ src/com/alansolidum/veritogp/VeritoGP.java
@@ -1,6 +1,11 @@
 package com.alansolidum.veritogp;
 
+import java.io.File;
+
 public class VeritoGP {
     public static void main(String[] args) {
+        File folder = new File("./");
+        VeritoFilelist fl = new VeritoFilelist();
+        fl.getAllFilesInFolder(folder);
     }
 }
