03c5ca6d7e2cc4da45b2e5c19f2376b8611b7b1b
Alan Solidum <hi@alansolidum.com>
2021-09-21 15:11:31 -0700

    feat: Add ability to determine earliest/latest file creation dates
    Google Photos API supports date ranges to filter the results returned.
    Thus determining the earliest and latest file creation dates in our dir
    file list will give us the dates we can feed into our filter
    
    Closes #1

--- src/main/java/com/alansolidum/veritogp/VeritoFilelist.java
+++ src/main/java/com/alansolidum/veritogp/VeritoFilelist.java
@@ -1,11 +1,21 @@
 package com.alansolidum.veritogp;
 
+import com.google.type.Date;
+
 import java.io.File;
 import java.io.FileFilter;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.nio.file.attribute.BasicFileAttributes;
+import java.time.LocalDate;
 import java.util.HashSet;
 import java.util.Set;
 
 public class VeritoFilelist {
+    private LocalDate startDate = LocalDate.MAX;
+    private LocalDate endDate = LocalDate.MIN;
+
     public boolean isValidExtension(String filename) {
         String fmtFilename = filename.toLowerCase();
 
@@ -18,9 +28,33 @@ public class VeritoFilelist {
                 fmtFilename.endsWith(".dng");
     }
 
-    public Set<String> getAllFilesInFolder(File folder) {
-        Set<String> files = new HashSet<>();
+    private LocalDate convertFileDateToLocalDate(File file) throws IOException {
+        BasicFileAttributes attr = Files.readAttributes(Paths.get(file.getAbsolutePath()),
+                BasicFileAttributes.class);
 
+        return LocalDate.ofEpochDay((attr.creationTime().toMillis()/1000));
+    }
+
+    // This code will convert the LocalDate type used in this class to the type
+    // needed in the Google Photos API filter date range
+    public Date convertLocalDateToGoogleDate (LocalDate date) {
+        return Date.newBuilder()
+                .setDay(date.getDayOfMonth())
+                .setMonth(date.getMonthValue())
+                .setYear(date.getYear())
+                .build();
+    }
+
+    public Date getStartDate() {
+        return convertLocalDateToGoogleDate(startDate);
+    }
+
+    public Date getEndDate() {
+        return convertLocalDateToGoogleDate(endDate);
+    }
+
+    public Set<String> getAllFilesInFolder(File folder) throws IOException {
+        Set<String> files = new HashSet<>();
         FileFilter extFilter = new FileFilter() {
             // Override accept method
             public boolean accept(File file) {
@@ -30,9 +64,15 @@ public class VeritoFilelist {
                 return isValidExtension(file.getName());
             }
         };
+
         for (File file : folder.listFiles(extFilter)) {
             if (!file.isDirectory()) {
                 files.add(file.getName());
+                LocalDate fileDate = convertFileDateToLocalDate(file);
+                if (fileDate.compareTo(startDate) < 0)
+                    startDate = fileDate;
+                if (fileDate.compareTo(endDate) > 0)
+                    endDate = fileDate;
             } else {
                 // Recurse through nested folders
                 files.addAll(getAllFilesInFolder(file));
