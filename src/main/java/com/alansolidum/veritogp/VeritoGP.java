cddcc511dfeadb5c93725fda913baadcafdd928c
Alan Solidum <hi@alansolidum.com>
2021-09-21 18:02:17 -0700

    fix: Add exception handling to main()

--- src/main/java/com/alansolidum/veritogp/VeritoGP.java
+++ src/main/java/com/alansolidum/veritogp/VeritoGP.java
@@ -1,11 +1,16 @@
 package com.alansolidum.veritogp;
 
 import java.io.File;
+import java.io.IOException;
 
 public class VeritoGP {
     public static void main(String[] args) {
-        File folder = new File("./");
-        VeritoFilelist fl = new VeritoFilelist();
-        fl.getAllFilesInFolder(folder);
+        try {
+            File folder = new File("./");
+            VeritoFilelist fl = new VeritoFilelist();
+            fl.getAllFilesInFolder(folder);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
