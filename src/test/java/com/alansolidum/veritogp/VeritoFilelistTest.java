667c10186acae8b0890a23cc9ca112fbed1df366
Alan Solidum <hi@alansolidum.com>
2021-09-21 17:36:42 -0700

    test: Add test for converting LocalDate to Google Date type
    This code will convert the LocalDate type used in this class to the type
    needed in the Google Photos API filter date range

--- src/test/java/com/alansolidum/veritogp/VeritoFilelistTest.java
+++ src/test/java/com/alansolidum/veritogp/VeritoFilelistTest.java
@@ -4,6 +4,9 @@ import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 
+import java.time.LocalDate;
+import com.google.type.Date;
+
 public class VeritoFilelistTest {
     VeritoFilelist fl;
 
@@ -20,6 +23,19 @@ public class VeritoFilelistTest {
 
         // Check invalid extensions
         Assert.assertFalse(fl.isValidExtension("test.java"));
+    }
 
+    @Test
+    public void VeritoTestConvertLocalDateToGoogleDate() {
+        LocalDate localDate = LocalDate.now();
+        Date googleDate = Date.newBuilder()
+                .setDay(localDate.getDayOfMonth())
+                .setMonth(localDate.getMonthValue())
+                .setYear(localDate.getYear())
+                .build();
+
+        Date tempDate = fl.convertLocalDateToGoogleDate(localDate);
+
+        Assert.assertEquals(tempDate, googleDate);
     }
 }
