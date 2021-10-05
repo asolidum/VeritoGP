5a4e8381ebcc9ac60ae932dd5a9b3b85bf2bbfe9
Alan Solidum <hi@alansolidum.com>
2021-10-04 23:17:02 -0700

    feat: Update main() with Google Photo verification functionality

--- src/main/java/com/alansolidum/veritogp/VeritoGP.java
+++ src/main/java/com/alansolidum/veritogp/VeritoGP.java
@@ -1,14 +1,49 @@
 package com.alansolidum.veritogp;
 
+import org.apache.log4j.PropertyConfigurator;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import java.io.File;
 import java.io.IOException;
+import java.util.Set;
 
 public class VeritoGP {
+    static Logger logger = LoggerFactory.getLogger(VeritoGP.class.getName());
+
     public static void main(String[] args) {
+        PropertyConfigurator.configure("./log4j.properties");
+
+        // Default path is current directory unless otherwise specified in command line arg
+        String path = "./";
+        if (args.length > 0)
+            path = args[0];
         try {
-            File folder = new File("./");
             VeritoFilelist fl = new VeritoFilelist();
-            fl.getAllFilesInFolder(folder);
+
+            File folder = new File(path);
+            logger.info("Compiling filelist in folder: {}", path);
+
+            Set<String> dirFilelist = fl.getAllFilesInFolder(folder);
+            int numFiles = dirFilelist.size();
+            if (numFiles == 0) {
+                logger.info("0 local files found (Exiting...)");
+                System.exit(0);
+            }
+            logger.info("{} local file(s) found ({} to {})",
+                    numFiles,
+                    fl.getStartDateString(),
+                    fl.getEndDateString());
+
+            logger.info("Checking Google Photos (GP) servers");
+            Set<String> apiPhotoList = VeritoAPI
+                    .getPhotolistByRange(fl.getStartDate(), fl.getEndDate());
+
+            logger.info("{} remote file(s) found in GP", apiPhotoList.size());
+
+            // Remove overlapping filenames in dirFileList and apiPhotoList
+            dirFilelist.removeAll(apiPhotoList);
+            logger.info("{} UNMATCHED local file(s) ({})", dirFilelist.size(), dirFilelist);
         } catch (IOException e) {
             e.printStackTrace();
         }
